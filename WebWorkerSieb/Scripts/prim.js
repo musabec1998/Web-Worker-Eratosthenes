d4d90583640e5921d78132a01f646f05bb918674
ita1-tn16 <ita1-tn16@PC0562>
2023-07-06 09:02:55 +0200

    Projektdateien hinzufügen.

---
+++ WebWorkerSieb/Scripts/prim.js
@@ -0,0 +1,42 @@
+﻿self.onmessage = function (evt)
+{
+    self.postMessage("Starte mit max: " + evt.data.maxnum);
+
+    var maximum = evt.data.maxnum;
+
+    var array = [true, true];
+
+    for (var i = 2; i <= maximum; i++)
+    {
+        array.push(false);
+    }
+
+    for (var i = 2; i <= Math.floor(Math.sqrt(maximum)); i++)
+    {
+        if (i < array.length)
+        {
+            if (array[i] === false)
+            {
+                self.postMessage({ type: "prime", value: i });
+
+                for (var j = i * i; j <= maximum; j += i)
+                {
+                    array[j] = true;
+
+                    self.postMessage({ type: "tested", value: j });
+                }
+            }
+        }
+    }
+
+    for (var i = Math.floor(Math.sqrt(maximum)) + 1; i <= maximum; i++)
+    {
+        if (i < array.length)
+        {
+            if (array[i] === false)
+            {
+                self.postMessage({ type: "prime", value: i });
+            }
+        }
+    }
+};
---
+++ WebWorkerSieb/Scripts/skript.js
@@ -0,0 +1,77 @@
+﻿var results = [];
+
+$(document).ready(function ()
+{
+    "use strict";
+
+    slider();
+
+    
+    $('#maxnum').on('input', function ()
+    {
+        slider();
+    });
+
+    var workertest;
+    var timer;
+
+    $('#starten').on('click', function ()
+    {
+        console.log('Click!');
+
+        slider();
+
+        workertest = new Worker('/Scripts/prim.js');
+
+        workertest.addEventListener('message', function (evt)
+        {
+            results.push(evt.data);
+        });
+
+        workertest.postMessage({ maxnum: $('#maxnum').val() });
+
+        timer = setInterval(function ()
+        {
+            if (results.length > 0)
+            {
+                var res = results.shift();
+
+                $('#' + res.value).addClass(res.type);
+
+                console.log(res);
+            }
+        }, 100);
+    });
+
+    $('#stopen').on('click', function ()
+    {
+        workertest.terminate();
+
+        clearInterval(timer);
+
+        results = [];
+    });
+});
+
+function slider()
+{
+    "use strict";
+
+    $('#rangeDisplay').text($('#maxnum').val());
+
+    zeigezahlen($('#maxnum').val());
+}
+
+function zeigezahlen(max)
+{
+    "use strict";
+
+    $('div.display').empty();
+
+    for (var i = 1; i <= max; i++)
+    {
+        $('<span>', { "class": "number", text: (i > 1) ? i : "", id: i }).appendTo('div.display');
+    }
+
+    $('<div>', { "style": "clear:both; width:0; height:0;" }).appendTo('div.display');
+}
